1877c49d7f3b90bbf2d0dff26a6b908b4a408c6f
ISRaiken <samueleorazio.durante@gmail.com>
2025-03-25 23:48:47 +0100

    fixato pulsante linux download, fixata barra laterale

--- patchnotes.php
+++ patchnotes.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Patch Notes</title>
     <link rel="stylesheet" href="css/styles_patchnotes.css">
+    <link rel="stylesheet" href="css/styles_scrollbar-styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 
--- roadmap.php
+++ roadmap.php
@@ -7,6 +7,7 @@
     <title>Project Roadmap</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="css/styles_roadmap.css">
+    <link rel="stylesheet" href="css/scrollbar-styles.css">
 </head>
 <body>
 
